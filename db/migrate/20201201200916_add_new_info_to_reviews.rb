eb5e9ce2297dbd92e38ed473bad4ea2f6f7e4feb
Meggy Barbier <meggybarbier@gmail.com>
2020-12-01 15:22:32 -0500

    added ambiance and SEO

---
+++ db/migrate/20201201200916_add_new_info_to_reviews.rb
@@ -0,0 +1,9 @@
+class AddNewInfoToReviews < ActiveRecord::Migration[6.0]
+  def change
+
+    add_column :reviews, :phone_number, :string
+    add_column :reviews, :ambiance, :string 
+
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_163312) do
+ActiveRecord::Schema.define(version: 2020_12_01_200916) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_11_08_163312) do
     t.string "cuisine"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone_number"
+    t.string "ambiance"
   end
 
 end
